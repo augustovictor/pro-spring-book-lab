3f30e5b0cda3b7239e7604be3b9eadbc3216f680
augustovictor <victoraweb@gmail.com>
2020-02-07 23:25:10 -0300

    [TEST] Add test for ContractNumberController

--- src/test/kotlin/com/github/augustovictor/prospringbooklab/contractNumber/ContractNumberControllerTest.kt
+++ src/test/kotlin/com/github/augustovictor/prospringbooklab/contractNumber/ContractNumberControllerTest.kt
@@ -1,18 +1,20 @@
 package com.github.augustovictor.prospringbooklab.contractNumber
 
+import com.github.augustovictor.prospringbooklab.converter.AppConfig
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.Import
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @RunWith(SpringRunner::class)
 @WebMvcTest(ContractNumberController::class)
+@Import(AppConfig::class)
 class ContractNumberControllerTest {
 
     @Autowired
@@ -24,6 +26,8 @@ class ContractNumberControllerTest {
 
         val request = MockMvcRequestBuilders
                 .get("/contract-number/$validContractNumber/validate")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
 
         mockMvc.perform(request)
                 .andExpect(status().isOk)
