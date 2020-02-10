de916d3a1a1adacac80cc99755f25a45ba45c4d2
augustovictor <victoraweb@gmail.com>
2020-02-10 06:28:03 -0300

    [FEAT] Add assertTrue

--- src/main/kotlin/com/github/augustovictor/prospringbooklab/movie/Movie.kt
+++ src/main/kotlin/com/github/augustovictor/prospringbooklab/movie/Movie.kt
@@ -1,10 +1,24 @@
 package com.github.augustovictor.prospringbooklab.movie
 
 import com.github.augustovictor.prospringbooklab.validator.CheckMovie
+import javax.validation.constraints.AssertTrue
 import javax.validation.constraints.NotNull
 
 @CheckMovie
 data class Movie(
         @NotNull
-        val title: String
-)
+        val title: String,
+
+        val description: String? = null
+) {
+        @AssertTrue(message = "")
+        fun thereIsDescription(): Boolean {
+                var isValid = true
+
+                if (description == null) {
+                        isValid = false
+                }
+
+                return isValid
+        }
+}
